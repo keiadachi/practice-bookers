5ef8054c3bb424847393784db6077cd9a8fa35f5
keiadachi <2013edcckeiadachi@gmail.com>
2021-03-23 18:57:25 +0000

    practice bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  
+  def index
+    @book = Book.new
+    #newbookへインスタンスを渡す
+    @books = Book.all
+    #tableへインスタンスを渡しeachで振り分け
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:book] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render action: :index
+    end
+  end
+
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        redirect_to book_path(@book.id)
+        flash[:book] = "update successfully"
+      else
+        render action: :edit
+      end 
+  end 
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    flash[:book] = "削除完了しました"
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
